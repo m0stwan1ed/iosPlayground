e30ede4652ed574f1a280916fc593b665192e563
Wladyslaw Mostowicz <m0stwan1ed@MacBook-Air-Wladyslaw.local>
2019-10-07 07:45:23 -0700

    initial commit

---
+++ Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+let stala:Int = 12
+let переменная:String = "Zmienna"
+
+var 😼 = "Cat"
+
+var lista = ["a","b","c"]
+
+func funkcja() -> Bool{
+    print("Hello, world!")
+    return false
+}
+
+class Czlowiek
+{
+    init (parameters) {
+        statements
+    }
+}
+
